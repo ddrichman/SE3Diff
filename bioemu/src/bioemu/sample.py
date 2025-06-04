de03151c4b9d38663a50914a04c169088bb99901
FridrichMethod <zhaoyangli@stanford.edu>
2025-06-04 12:40:50 -0700

    Refactor sample.py

--- bioemu/src/bioemu/sample.py
+++ bioemu/src/bioemu/sample.py
@@ -13,7 +13,6 @@ import fire
 import hydra
 import numpy as np
 import torch
-import torch.nn as nn
 import yaml
 from huggingface_hub import hf_hub_download
 from torch._prims_common import DeviceLikeType
@@ -22,11 +21,12 @@ from tqdm import tqdm
 
 from bioemu.chemgraph import ChemGraph
 from bioemu.convert_chemgraph import save_pdb_and_xtc
-from bioemu.denoiser import DenoisedSDEPath, SDEs
+from bioemu.denoiser import SDEs
 from bioemu.get_embeds import get_colabfold_embeds
 from bioemu.models import DiGConditionalScoreModel
 from bioemu.seq_io import check_protein_valid, parse_sequence, write_fasta
 from bioemu.utils import (
+    clean_gpu_cache,
     count_samples_in_output_dir,
     format_npz_samples_filename,
     print_traceback_on_exception,
@@ -35,7 +35,7 @@ from bioemu.utils import (
 logger = logging.getLogger(__name__)
 
 # Denoiser config
-DEFAULT_DENOISER_CONFIG_DIR = Path(__file__).parent / "config/denoiser/"
+DEFAULT_DENOISER_CONFIG_DIR = Path(__file__).parent / "config/denoiser"
 SupportedDenoisersLiteral = Literal["dpm", "heun", "euler_maruyama"]
 SUPPORTED_DENOISERS = list(typing.get_args(SupportedDenoisersLiteral))
 
@@ -43,6 +43,12 @@ SUPPORTED_DENOISERS = list(typing.get_args(SupportedDenoisersLiteral))
 DEFAULT_MODEL_CHECKPOINT_DIR = Path(__file__).parent / "checkpoints"
 SupportedModelNamesLiteral = Literal["bioemu-v1.0", "bioemu-rev"]
 SUPPORTED_MODEL_NAMES = list(typing.get_args(SupportedModelNamesLiteral))
+
+
+class Bundle(NamedTuple):
+    sdes: SDEs
+    score_model: DiGConditionalScoreModel
+    denoiser: Callable
 
 
 def maybe_download_checkpoint(
@@ -57,6 +63,7 @@ def maybe_download_checkpoint(
     """
 
     if model_name is None:
+        logger.info("No model_name specified, using ckpt_path and model_config_path.")
         if ckpt_path is None:
             raise ValueError("If model_name is not specified, you must provide ckpt_path.")
         if model_config_path is None:
@@ -96,6 +103,41 @@ def maybe_download_checkpoint(
         logger.info(f"Copied model config to {model_config_path}")
 
     return str(ckpt_path), str(model_config_path)
+
+
+def load_bundle(
+    model_name: SupportedModelNamesLiteral | None = "bioemu-v1.0",
+    ckpt_path: str | Path | None = None,
+    model_config_path: str | Path | None = None,
+    denoiser_type: SupportedDenoisersLiteral | None = "dpm",
+    denoiser_config_path: str | Path | None = None,
+    cache_so3_dir: str | Path | None = None,
+) -> Bundle:
+    ckpt_path, model_config_path = maybe_download_checkpoint(
+        model_name=model_name, ckpt_path=ckpt_path, model_config_path=model_config_path
+    )
+
+    with open(model_config_path) as f:
+        model_config = yaml.safe_load(f)
+
+    if cache_so3_dir is not None:
+        model_config["sdes"]["node_orientations"]["cache_dir"] = cache_so3_dir
+
+    model_state = torch.load(ckpt_path, map_location="cpu", weights_only=True)
+    score_model: DiGConditionalScoreModel = hydra.utils.instantiate(model_config["score_model"])
+    score_model.load_state_dict(model_state)
+    sdes: SDEs = hydra.utils.instantiate(model_config["sdes"])
+
+    if denoiser_config_path is None:
+        if denoiser_type not in SUPPORTED_DENOISERS:
+            raise ValueError(f"denoiser_type must be one of {SUPPORTED_DENOISERS}")
+        denoiser_config_path = DEFAULT_DENOISER_CONFIG_DIR / f"{denoiser_type}.yaml"
+
+    with open(denoiser_config_path) as f:
+        denoiser_config = yaml.safe_load(f)
+    denoiser: Callable = hydra.utils.instantiate(denoiser_config)
+
+    return Bundle(sdes=sdes, score_model=score_model, denoiser=denoiser)
 
 
 def generate_chemgraph(
@@ -141,12 +183,11 @@ def generate_chemgraph(
     return chemgraph
 
 
+@clean_gpu_cache
 @torch.no_grad()
 def generate_batch(
     sequence: str,
-    sdes: SDEs,
-    score_model: nn.Module,
-    denoiser: Callable,
+    bundle: Bundle,
     batch_size: int,
     device: DeviceLikeType | None = None,
     cache_embeds_dir: str | Path | None = None,
@@ -181,13 +222,15 @@ def generate_batch(
     )
     batch = Batch.from_data_list([chemgraph for _ in range(batch_size)])
 
+    sdes, score_model, denoiser = bundle
+
     sampled_chemgraph_batch = denoiser(
         batch=batch,
         sdes=sdes,
         score_model=score_model,
         device=device,
     )
-    assert isinstance(sampled_chemgraph_batch, Batch)
+
     sampled_chemgraphs = sampled_chemgraph_batch.to_data_list()
     pos = torch.stack([x.pos for x in sampled_chemgraphs]).to("cpu")
     node_orientations = torch.stack([x.node_orientations for x in sampled_chemgraphs]).to("cpu")
@@ -195,54 +238,93 @@ def generate_batch(
     return {"pos": pos, "node_orientations": node_orientations}
 
 
-@torch.no_grad()
-def generate_finetune_batch(
-    sequence: str,
-    sdes: SDEs,
-    score_model: nn.Module,
-    finetune_model: nn.Module,
-    denoiser: Callable,
-    batch_size: int,
+def sample(
+    sequence: str | Path,
+    output_dir: str | Path,
+    num_samples: int,
+    bundle: Bundle,
+    batch_size_100: int = 10,
     device: DeviceLikeType | None = None,
     cache_embeds_dir: str | Path | None = None,
     msa_file: str | Path | None = None,
     msa_host_url: str | None = None,
-    seed: int | None = None,
-) -> DenoisedSDEPath:
-    """Generate one batch of samples, using GPU if available.
+    filter_samples: bool = True,
+):
 
-    Args:
-        sequence: Amino acid sequence.
-        sdes: SDEs defining corruption process. Keys should be 'node_orientations' and 'pos'.
-        score_model: Score model.
-        finetune_model: Finetune model.
-        denoiser: Denoiser function.
-        batch_size: Batch size.
-        device: Device to use for sampling. If not set, this defaults to the current device.
-        cache_embeds_dir: Directory to store MSA embeddings. If not set, this defaults to `COLABFOLD_DIR/embeds_cache`.
-        msa_file: Optional path to an MSA A3M file.
-        msa_host_url: MSA server URL for colabfold.
-        seed: Random seed.
-    """
+    # Parse FASTA or A3M file if sequence is a file path. Extract the actual sequence.
+    sequence = parse_sequence(sequence)
 
-    if seed is not None:
-        torch.manual_seed(seed)
+    # Check input sequence is valid
+    check_protein_valid(sequence)
 
-    chemgraph = generate_chemgraph(
+    if msa_file is not None and msa_host_url is not None:
+        logger.warning(f"msa_host_url is ignored because MSA file {msa_file} is provided.")
+
+    output_dir = Path(output_dir).expanduser().resolve()
+    output_dir.mkdir(parents=True, exist_ok=True)  # Fail fast if output_dir is non-writeable
+
+    fasta_path = output_dir / "sequence.fasta"
+    if fasta_path.is_file():
+        if parse_sequence(fasta_path) != sequence:
+            raise ValueError(
+                f"{fasta_path} already exists, but contains a sequence different from {sequence}!"
+            )
+    else:
+        # Save FASTA file in output_dir
+        write_fasta([sequence], fasta_path)
+
+    logger.info(
+        f"Sampling {num_samples} structures for sequence of length {len(sequence)} residues..."
+    )
+    batch_size = int(batch_size_100 * (100 / len(sequence)) ** 2)
+    if batch_size == 0:
+        logger.warning(f"Sequence {sequence} may be too long. Attempting with batch_size = 1.")
+        batch_size = 1
+    logger.info(f"Using batch size {min(batch_size, num_samples)}")
+
+    existing_num_samples = count_samples_in_output_dir(output_dir)
+    logger.info(f"Found {existing_num_samples} previous samples in {output_dir}.")
+
+    for seed in tqdm(
+        range(existing_num_samples, num_samples, batch_size), desc="Sampling batches..."
+    ):
+        n = min(batch_size, num_samples - seed)
+        npz_path = output_dir / format_npz_samples_filename(seed, n)
+        if npz_path.exists():
+            raise ValueError(
+                f"Not sure why {npz_path} already exists when so far only {existing_num_samples} samples have been generated."
+            )
+        logger.info(f"Sampling {seed=}")
+        batch = generate_batch(
+            sequence=sequence,
+            bundle=bundle,
+            batch_size=min(batch_size, n),
+            device=device,
+            cache_embeds_dir=cache_embeds_dir,
+            msa_file=msa_file,
+            msa_host_url=msa_host_url,
+            seed=seed,
+        )
+        np.savez(npz_path, **{k: v.cpu().numpy() for k, v in batch.items()}, sequence=sequence)
+
+    logger.info("Converting samples to .pdb and .xtc...")
+    samples_files = sorted(list(output_dir.glob("batch_*.npz")))
+    sequences = [np.load(f)["sequence"].item() for f in samples_files]
+    if set(sequences) != {sequence}:
+        raise ValueError(f"Expected all sequences to be {sequence}, but got {set(sequences)}")
+    positions = torch.tensor(np.concatenate([np.load(f)["pos"] for f in samples_files]))
+    node_orientations = torch.tensor(
+        np.concatenate([np.load(f)["node_orientations"] for f in samples_files])
+    )
+    save_pdb_and_xtc(
+        pos_nm=positions,
+        node_orientations=node_orientations,
         sequence=sequence,
-        cache_embeds_dir=cache_embeds_dir,
-        msa_file=msa_file,
-        msa_host_url=msa_host_url,
+        topology_path=output_dir / "topology.pdb",
+        xtc_path=output_dir / "samples.xtc",
+        filter_samples=filter_samples,
     )
-    batch = Batch.from_data_list([chemgraph for _ in range(batch_size)])
-
-    return denoiser(
-        batch=batch,
-        sdes=sdes,
-        score_model=score_model,
-        finetune_model=finetune_model,
-        device=device,
-    )
+    logger.info(f"Completed. Your samples are in {output_dir}.")
 
 
 @print_traceback_on_exception
@@ -287,105 +369,27 @@ def main(
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    output_dir = Path(output_dir).expanduser().resolve()
-    output_dir.mkdir(parents=True, exist_ok=True)  # Fail fast if output_dir is non-writeable
-
-    ckpt_path, model_config_path = maybe_download_checkpoint(
-        model_name=model_name, ckpt_path=ckpt_path, model_config_path=model_config_path
+    bundle = load_bundle(
+        model_name=model_name,
+        ckpt_path=ckpt_path,
+        model_config_path=model_config_path,
+        denoiser_type=denoiser_type,
+        denoiser_config_path=denoiser_config_path,
+        cache_so3_dir=cache_so3_dir,
     )
 
-    with open(model_config_path) as f:
-        model_config = yaml.safe_load(f)
-
-    if cache_so3_dir is not None:
-        model_config["sdes"]["node_orientations"]["cache_dir"] = cache_so3_dir
-
-    if msa_file is not None and msa_host_url is not None:
-        logger.warning(f"msa_host_url is ignored because MSA file {msa_file} is provided.")
-
-    # Parse FASTA or A3M file if sequence is a file path. Extract the actual sequence.
-    sequence = parse_sequence(sequence)
-
-    # Check input sequence is valid
-    check_protein_valid(sequence)
-
-    fasta_path = output_dir / "sequence.fasta"
-    if fasta_path.is_file():
-        if parse_sequence(fasta_path) != sequence:
-            raise ValueError(
-                f"{fasta_path} already exists, but contains a sequence different from {sequence}!"
-            )
-    else:
-        # Save FASTA file in output_dir
-        write_fasta([sequence], fasta_path)
-
-    model_state = torch.load(ckpt_path, map_location="cpu", weights_only=True)
-    score_model: DiGConditionalScoreModel = hydra.utils.instantiate(model_config["score_model"])
-    score_model.load_state_dict(model_state)
-    sdes: SDEs = hydra.utils.instantiate(model_config["sdes"])
-
-    if denoiser_config_path is None:
-        if denoiser_type not in SUPPORTED_DENOISERS:
-            raise ValueError(f"denoiser_type must be one of {SUPPORTED_DENOISERS}")
-        denoiser_config_path = DEFAULT_DENOISER_CONFIG_DIR / f"{denoiser_type}.yaml"
-
-    with open(denoiser_config_path) as f:
-        denoiser_config = yaml.safe_load(f)
-    denoiser: Callable = hydra.utils.instantiate(denoiser_config)
-
-    logger.info(
-        f"Sampling {num_samples} structures for sequence of length {len(sequence)} residues..."
-    )
-    batch_size = int(batch_size_100 * (100 / len(sequence)) ** 2)
-    if batch_size == 0:
-        logger.warning(f"Sequence {sequence} may be too long. Attempting with batch_size = 1.")
-        batch_size = 1
-    logger.info(f"Using batch size {min(batch_size, num_samples)}")
-
-    existing_num_samples = count_samples_in_output_dir(output_dir)
-    logger.info(f"Found {existing_num_samples} previous samples in {output_dir}.")
-    for seed in tqdm(
-        range(existing_num_samples, num_samples, batch_size), desc="Sampling batches..."
-    ):
-        n = min(batch_size, num_samples - seed)
-        npz_path = output_dir / format_npz_samples_filename(seed, n)
-        if npz_path.exists():
-            raise ValueError(
-                f"Not sure why {npz_path} already exists when so far only {existing_num_samples} samples have been generated."
-            )
-        logger.info(f"Sampling {seed=}")
-        batch = generate_batch(
-            sequence=sequence,
-            sdes=sdes,
-            score_model=score_model,
-            denoiser=denoiser,
-            batch_size=min(batch_size, n),
-            device=device,
-            cache_embeds_dir=cache_embeds_dir,
-            msa_file=msa_file,
-            msa_host_url=msa_host_url,
-            seed=seed,
-        )
-        np.savez(npz_path, **{k: v.cpu().numpy() for k, v in batch.items()}, sequence=sequence)
-
-    logger.info("Converting samples to .pdb and .xtc...")
-    samples_files = sorted(list(output_dir.glob("batch_*.npz")))
-    sequences = [np.load(f)["sequence"].item() for f in samples_files]
-    if set(sequences) != {sequence}:
-        raise ValueError(f"Expected all sequences to be {sequence}, but got {set(sequences)}")
-    positions = torch.tensor(np.concatenate([np.load(f)["pos"] for f in samples_files]))
-    node_orientations = torch.tensor(
-        np.concatenate([np.load(f)["node_orientations"] for f in samples_files])
-    )
-    save_pdb_and_xtc(
-        pos_nm=positions,
-        node_orientations=node_orientations,
-        topology_path=output_dir / "topology.pdb",
-        xtc_path=output_dir / "samples.xtc",
+    sample(
         sequence=sequence,
+        output_dir=output_dir,
+        num_samples=num_samples,
+        bundle=bundle,
+        batch_size_100=batch_size_100,
+        device=device,
+        cache_embeds_dir=cache_embeds_dir,
+        msa_file=msa_file,
+        msa_host_url=msa_host_url,
         filter_samples=filter_samples,
     )
-    logger.info(f"Completed. Your samples are in {output_dir}.")
 
 
 if __name__ == "__main__":
